6e55de09e9ba121c291e7492c4d0ed05eb3c921f
Lyle Goff <johngoff18@gmail.com>
2019-05-03 13:39:29 -0400

    Added AJAX to cart in sidebar page 171

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
-  def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
     end
-  end
 
 end
